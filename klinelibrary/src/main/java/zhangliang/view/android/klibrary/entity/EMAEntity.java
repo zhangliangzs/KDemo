cad2f9b89c1b0ea386fe3e1eb7a389497ab641ef
JackySheng <shengqiang1001@gmail.com>
2017-02-15 13:13:20 +0800

    修改MA均线、macd

--- klinelibrary/src/main/java/zhangliang/view/android/klibrary/entity/EMAEntity.java
+++ klinelibrary/src/main/java/zhangliang/view/android/klibrary/entity/EMAEntity.java
@@ -12,7 +12,31 @@ public class EMAEntity {
 
     public EMAEntity(List<MarketChartData> OHLCData, int n)
     {
+
+
+
+
         EMAs = new ArrayList<Double>();
+        if (OHLCData != null && OHLCData.size() > 0) {
+            double close = 0;
+            double EMA12 = 0.0;
+            MarketChartData oHLCEntity;
+            for (int i = 0; i <= OHLCData.size() - 1; i++) {
+                close = OHLCData.get(i).getClosePrice();
+                oHLCEntity = OHLCData.get(i);
+                if (i ==0) {
+                    EMA12 = oHLCEntity.getClosePrice();
+                    EMAs.add(EMA12);
+                }else
+                {
+                    EMA12 = EMAs.get(EMAs.size()-1)*(n-1)/(n+1)+close*(2)/(n+1);
+                    EMAs.add(EMA12);
+                }
+            }
+
+        }
+
+  /*      EMAs = new ArrayList<Double>();
         if (OHLCData != null && OHLCData.size() > 0) {
             double close = 0;
             double EMA12 = 0.0;
@@ -30,7 +54,7 @@ public class EMAEntity {
                 }
             }
 
-        }
+        }*/
     }
     public List<Double> getnEMA() {
         return EMAs;
